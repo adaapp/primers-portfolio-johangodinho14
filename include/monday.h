8fa114c71ee95dd279799db5d340e7bae8ae2785
Jesus Johan Godinho <53049127+johangodinho14@users.noreply.github.com>
2021-08-17 11:49:53 +0000

    Primer 1 - Indentation fix.

--- include/monday.h
+++ include/monday.h
@@ -103,7 +103,7 @@ void convertTemperature(void){
 }
 
 void fahrenheitCentigradeConversion(void) {
-  std::cout<<std::fixed<<std::setprecision(2);
+  std::cout<<std::fixed<<std::setprecision(2); 
 	convertTemperature();
 }
 
