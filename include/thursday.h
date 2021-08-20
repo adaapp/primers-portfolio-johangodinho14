baa5272ff4f70e7a97f7b5248c814a9dcfb7c444
Jesus Johan Godinho <53049127+johangodinho14@users.noreply.github.com>
2021-08-20 10:38:46 +0000

    Primer-7 - Version 1 completed.

--- include/thursday.h
+++ include/thursday.h
@@ -1,8 +1,22 @@
+#include <iostream>
+#include<thread>
+
+//Sleep for a given amount of seconds
+//Deaults to 10 seconds if no argument is provided
+void sleep(int seconds = 10){
+    //Calculating seconds in ms
+    int ms = seconds * 1000;
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
 void sleepTimer(void) {
-	std::cout << " - sleepTimer: not yet implemented\n\n";
+  std::cout<<"Sleeping for 5 seconds\n";
+  sleep(5);
+  std::cout<<"Finished sleeping for 5 seconds";
 }
 
 
+// ------------------  Join detach threads ------------------------------------
 void joinDetachThreads(void) {
 	std::cout << " - joinDetachThreads: not yet implemented\n\n";
 }
